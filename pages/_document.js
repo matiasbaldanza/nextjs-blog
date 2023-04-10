e64e88a89c995542dd91467b23a104a956c20f51
Matias Baldanza <matiasbaldanza@gmail.com>
2023-04-10 19:08:52 -0300

    feat: basic home, layout. Add list of posts via getStaticProps

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document(){
+    return (
+        <Html>
+            <Head>
+                <title>Mi Blogcito</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <body>
+                <Main>
+                </Main>
+                <NextScript>
+                </NextScript>
+            </body>
+        </Html>
+    )
+}
